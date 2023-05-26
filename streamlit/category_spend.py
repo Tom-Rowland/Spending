87bb823819615d7f341e3ca280a8b27d1c362079
Tom Rowland <tomwdrowland@gmail.com>
2023-05-26 19:41:14 +0100

    don't force markdown text to be black

--- streamlit/category_spend.py
+++ streamlit/category_spend.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def main(cat_spend):
-    st.markdown(f"<h2 style='text-align: center; color: black;'>£{cat_spend}</h2>", unsafe_allow_html=True)
-    st.markdown(f"<p style='text-align: center; color: black;'>Spent across selected categories</p>", unsafe_allow_html=True)
+    st.markdown(f"<h2 style='text-align: center; '>£{cat_spend}</h2>", unsafe_allow_html=True)
+    st.markdown(f"<p style='text-align: center;'>Spent across selected categories</p>", unsafe_allow_html=True)
