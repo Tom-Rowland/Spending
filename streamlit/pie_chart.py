6a9539c95cfffe1fcea038e12ab479f6a6e4f148
Tom Rowland <tomwdrowland@gmail.com>
2023-05-26 19:12:50 +0100

    Show subcategories in pie chart if only one category selected

--- streamlit/pie_chart.py
+++ streamlit/pie_chart.py
@@ -2,7 +2,10 @@ import streamlit as st
 import plotly.graph_objects as go
 
 def main(selected_df):
-    df_grouped = selected_df[['Category','Amount']].groupby('Category').sum()
+    if 'Subcategory' in selected_df.columns and len(set(selected_df['Category']))==1:
+        df_grouped = selected_df[['Subcategory','Amount']].groupby('Subcategory').sum()
+    else:
+        df_grouped = selected_df[['Category','Amount']].groupby('Category').sum()
 
     fig_labels = df_grouped.index
     fig_values = df_grouped['Amount']
