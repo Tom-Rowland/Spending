8a4c5cd46ae60f3cf7b934cc60ef29fb931eb819
Tom Rowland <tomwdrowland@gmail.com>
2023-05-26 17:40:35 +0100

    remove support for xlsx

--- streamlit/upload.py
+++ streamlit/upload.py
@@ -2,15 +2,12 @@ import streamlit as st
 import pandas as pd
 
 def main():
-    uploaded_file = st.sidebar.file_uploader("Please upload a file", type=['csv', 'xlsx'])
+    uploaded_file = st.sidebar.file_uploader("Please upload a file", type=['csv'])
     if uploaded_file:
-        if uploaded_file.type =='text/csv':
-            df = pd.read_csv(uploaded_file, parse_dates=['Date'])
-        else:
-            df = pd.read_excel(uploaded_file, parse_dates=['Date'])
-    else: 
+        df = pd.read_csv(uploaded_file, parse_dates=['Date'])
+    else:
         df = pd.DataFrame(columns=['Date','Category','Subcategory','Amount',])
 
-    if df['Date'].dtype == 'str':
+    if df['Date'].dtype in ['str','object']:
         df['Date'] = pd.to_datetime(df['Date'],dayfirst=True)
     return df
