789111e0b802907dc1e0539d2a33e3626ffe3a72
Tom <tomzarowland@gmail.com>
2023-07-22 14:07:54 +0100

    Add personalStarling to combine

--- statement_processing/combine.py
+++ statement_processing/combine.py
@@ -3,11 +3,13 @@ import pandas as pd
 if __name__ == '__main__':
     personal = pd.read_csv('data/4 staging to combine/HSBC.csv')
     joint = pd.read_csv('data/4 staging to combine/Starling.csv')
+    personalStarling = pd.read_csv('data/4 staging to combine/personalStarling.csv')
 
     personal = personal.loc[:,['Date','Description','Amount','Category','Subcategory']]
     joint = joint.loc[:,['Date','Description','Amount','Category','Subcategory']]
+    personalStarling = personalStarling.loc[:,['Date','Description','Amount','Category','Subcategory']]
 
-    combined = pd.concat([personal,joint], ignore_index=True)
+    combined = pd.concat([personal,joint,personalStarling], ignore_index=True)
     combined['Date'] = pd.to_datetime(combined['Date'],dayfirst=True)
     combined.sort_values(by='Date')
 
