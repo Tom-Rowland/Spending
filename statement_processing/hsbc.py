7797421f741abb16f0b8922d4e9ef3de0e7d735c
Tom <tomzarowland@gmail.com>
2023-07-22 12:20:18 +0100

    Finish HSBC

--- statement_processing/hsbc.py
+++ statement_processing/hsbc.py
@@ -1,22 +1,28 @@
 import pandas as pd
-import categorise
+import mapping
 
 if __name__ == '__main__':
     statement = pd.read_csv('data/1 raw/TransactionHistory.csv',header=None)
     statement.columns = ['Date', 'Description','Amount']
+    statement['Amount'] = statement['Amount'].str.replace(',', '').astype(float)
     statement['Description'] = statement['Description'].apply(lambda x: x.replace(" )))",""))
     statement['Category'], statement['Subcategory'] = None, None
 
     for index, transaction in statement.iterrows():
         #Standard mapping
-        category, subcategory = categorise.standard_mapping(transaction['Description'])
+        category, subcategory = mapping.mapping(transaction['Description'])
         statement.at[index, 'Category'] = category
         statement.at[index, 'Subcategory'] = subcategory
+    
     # For food at office, determine if breakfast/lunch
-
+    food_in_office_empty_subcategory = (statement['Category'] == 'Food in office') & (statement['Subcategory'] == '')
+    statement.loc[food_in_office_empty_subcategory & (abs(statement['Amount']) < 2.50), 'Subcategory'] = 'Breakfast'
+    statement.loc[food_in_office_empty_subcategory & (abs(statement['Amount']) >= 2.50), 'Subcategory'] = 'Lunch'
+    
     # Label groceries from HSBC as 'Solo'
     statement.loc[statement['Category'] == 'Groceries', 'Subcategory'] = 'Solo'
 
     # Ignore transfers to starling accounts, not real expenditure
     statement = statement.loc[~statement['Description'].isin(['Thomas Rowland Cycle BP', 'MeganTom Joint Topup BP'])]
     statement.to_csv('data/2 autoprocessed/AutoProcessedTransactionHistory.csv')
+    statement.to_csv('data/3 manual changes/HSBC.csv')
