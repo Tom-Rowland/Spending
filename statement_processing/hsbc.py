0f26637de84609be26f7e81aff3ae45cf3cda3e3
Tom <tomzarowland@gmail.com>
2023-07-22 09:48:04 +0100

    More mapping

--- statement_processing/hsbc.py
+++ statement_processing/hsbc.py
@@ -1,19 +1,22 @@
 import pandas as pd
 import categorise
 
-statement = pd.read_csv('data/1 raw/TransactionHistory.csv',header=None)
-statement.columns = ['Date', 'Description','Amount']
-statement['Description'] = statement['Description'].apply(lambda x: x.replace(" )))",""))
-statement['Category'], statement['Subcategory'] = None, None
+if __name__ == '__main__':
+    statement = pd.read_csv('data/1 raw/TransactionHistory.csv',header=None)
+    statement.columns = ['Date', 'Description','Amount']
+    statement['Description'] = statement['Description'].apply(lambda x: x.replace(" )))",""))
+    statement['Category'], statement['Subcategory'] = None, None
 
-for index, transaction in statement.iterrows():
-    #Standard mapping
-    category, subcategory = categorise.standard_mapping(transaction['Description'])
-    statement.at[index, 'Category'] = category
-    statement.at[index, 'Subcategory'] = subcategory
-
+    for index, transaction in statement.iterrows():
+        #Standard mapping
+        category, subcategory = categorise.standard_mapping(transaction['Description'])
+        statement.at[index, 'Category'] = category
+        statement.at[index, 'Subcategory'] = subcategory
     # For food at office, determine if breakfast/lunch
 
+    # Label groceries from HSBC as 'Solo'
     statement.loc[statement['Category'] == 'Groceries', 'Subcategory'] = 'Solo'
 
-statement.to_csv('data/2 autoprocessed/AutoProcessedTransactionHistory.csv')
+    # Ignore transfers to starling accounts, not real expenditure
+    statement = statement.loc[~statement['Description'].isin(['Thomas Rowland Cycle BP', 'MeganTom Joint Topup BP'])]
+    statement.to_csv('data/2 autoprocessed/AutoProcessedTransactionHistory.csv')
