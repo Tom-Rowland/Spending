71ff531e591370ce4f3df3beaa77f032900a2998
Tom Rowland <tomwdrowland@gmail.com>
2023-07-22 07:52:41 +0100

    Init transaction processing

---
+++ statement_processing/hsbc.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('data/raw/TransactionHistory (1).csv',header=None)
+print(df.iloc[0])
+df.columns = ['Date', 'Description','Amount']
+print(df.iloc[0:5])
+df['Description'] = df['Description'].apply(lambda x: x.replace(" )))",""))
+
+print(df.iloc[0:5])
